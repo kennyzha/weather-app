450dfee6eebd1b6019eb4e31384956ff9d416fae
Kenny <kzhao7301@gmail.com>
2018-03-18 17:30:44 -0400

    created components

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Title from "./components/Title"
+import Form from "./components/Form"
+import Weather from "./components/Weather"
+
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <Title />
+        <Form />
+        <Weather />
+      </div>
+    );
+  }
+};
+
+
+export default App;
